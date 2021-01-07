2f0e06836fad9f2def55484024e397908c27149e
Luoming Xu <xjy46566696@gmail.com>
2021-01-07 14:46:07 +0800

    init commit

---
+++ src/com/github/luomingxu/idea/NotifyUtil.java
@@ -0,0 +1,16 @@
+package com.github.luomingxu.idea;
+
+import com.intellij.notification.NotificationGroup;
+import com.intellij.notification.NotificationGroupManager;
+import com.intellij.notification.Notifications;
+import com.intellij.openapi.ui.MessageType;
+
+public class NotifyUtil {
+
+    private static final NotificationGroup notificationGroup =
+            NotificationGroupManager.getInstance().getNotificationGroup("Luo Notification Group");
+
+    public static void info(String msg) {
+        Notifications.Bus.notify(notificationGroup.createNotification(msg, MessageType.INFO));
+    }
+}
